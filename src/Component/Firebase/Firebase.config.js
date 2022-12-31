df371a0bc2f95020000d29f5ab573cf02bafeaa8
FFrajibraj779 <mdrajikuli757@gmail.com>
2022-12-31 19:43:07 +0600

    stripe added

--- src/Component/Firebase/Firebase.config.js
+++ src/Component/Firebase/Firebase.config.js
@@ -1,23 +1,23 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   projectId: process.env.REACT_APP_projectId,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId,
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
-// export default app ;
+export default app ;
 
 
 
@@ -27,21 +27,21 @@
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCkR8H2BgsFoZn9Uu3hinBsB0QFnfKnJqk",
-  authDomain: "assignment10-c02d5.firebaseapp.com",
-  projectId: "assignment10-c02d5",
-  storageBucket: "assignment10-c02d5.appspot.com",
-  messagingSenderId: "576700154483",
-  appId: "1:576700154483:web:dae1321ecece919ac6a006"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCkR8H2BgsFoZn9Uu3hinBsB0QFnfKnJqk",
+//   authDomain: "assignment10-c02d5.firebaseapp.com",
+//   projectId: "assignment10-c02d5",
+//   storageBucket: "assignment10-c02d5.appspot.com",
+//   messagingSenderId: "576700154483",
+//   appId: "1:576700154483:web:dae1321ecece919ac6a006"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-export default app;
+// export default app;
